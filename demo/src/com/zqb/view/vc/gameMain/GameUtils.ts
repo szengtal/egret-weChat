70f81076fa415af14951ac693da91bbc934ca2bc
shisiyupeng <shisiyupeng@qq.com>
2018-01-22 23:28:38 +0800

    去掉测试用

--- demo/src/com/zqb/view/vc/gameMain/GameUtils.ts
+++ demo/src/com/zqb/view/vc/gameMain/GameUtils.ts
@@ -42,17 +42,17 @@ export class GameUtils {
 	// console.log("基于矩形的碰撞检测1",obj2.x);
 	// 	console.log("基于矩形的碰撞检测22",obj2.y);
 
-		     var shp:egret.Shape = new egret.Shape();
-        shp.graphics.beginFill( 0xff0000, 0.5);
-        shp.graphics.drawRect( obj1.x-rect1.width/2, obj1.y-rect1.height/2, rect1.width, rect1.height );
-        shp.graphics.endFill();
-        testContain.addChild( shp );
+		//      var shp:egret.Shape = new egret.Shape();
+        // shp.graphics.beginFill( 0xff0000, 0.5);
+        // shp.graphics.drawRect( obj1.x-rect1.width/2, obj1.y-rect1.height/2, rect1.width, rect1.height );
+        // shp.graphics.endFill();
+        // testContain.addChild( shp );
 
-		  var shp1:egret.Shape = new egret.Shape();
-        shp1.graphics.beginFill( 0xfcc0c0, 0.5);
-        shp1.graphics.drawRect( obj2.x, obj2.y, rect2.width, rect2.height );
-        shp1.graphics.endFill();
-        testContain.addChild( shp1 );
+		//   var shp1:egret.Shape = new egret.Shape();
+        // shp1.graphics.beginFill( 0xfcc0c0, 0.5);
+        // shp1.graphics.drawRect( obj2.x, obj2.y, rect2.width, rect2.height );
+        // shp1.graphics.endFill();
+        // testContain.addChild( shp1 );
 		
 		// console.log("基于矩形的碰撞检测2",rect2);
 		
